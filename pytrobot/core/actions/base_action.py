493bea01295492ee4af958373ce75e0fd4accc64
Matheus C. Delfino <matheus.corteletti@wmtrading.com.br>
2024-01-26 18:20:41 +0000

    metodos get_action e get_tool através de injeção nas classes bases

--- pytrobot/core/actions/base_action.py
+++ pytrobot/core/actions/base_action.py
@@ -6,6 +6,11 @@ class BaseAction(ABC):
     """
     Classe abstrata base para todas as ações no PyTRobot.
     """
+    def __init__(self, objects_layer):
+        self.objects_layer = objects_layer
+
+    def get_tool(self, tool_class):
+        return self.objects_layer.get_tool(tool_class)
 
     @abstractmethod
     def perform(self):
--- pytrobot/core/states/base_state.py
+++ pytrobot/core/states/base_state.py
@@ -2,8 +2,12 @@ from abc import ABC, abstractmethod
 
 
 class BaseState(ABC):
-    def __init__(self):
-        self._status = None
+    def __init__(self, objects_layer):
+        self._status = None,
+        self.objects_layer = objects_layer
+
+    def get_action(self, action_class):
+        return self.objects_layer.get_action(action_class)
 
     @abstractmethod
     def execute(self):
