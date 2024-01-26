fcf4f723d7e3366c8eb8a34ca5982c9080e989d1
Matheus C. Delfino <matheus.corteletti@wmtrading.com.br>
2024-01-26 19:42:37 +0000

    BaseTool auto-contido

--- pytrobot/core/tools/base_tool.py
+++ pytrobot/core/tools/base_tool.py
@@ -4,6 +4,25 @@ class BaseTool(ABC):
     """
     Classe abstrata base para todas as ferramentas no PyTRobot.
     """
+    def __init__(self):
+        self._data = None
+        self._status = None
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @property
+    def status(self):
+        return self._status
+
+    @status.setter
+    def status(self, value):
+        self._status = value
 
     @abstractmethod
     def use(self):
